5e40ec40b245d6ea8cbe552bac575666912918f5
sun-glass <68639801+sun-glass@users.noreply.github.com>
2020-12-08 10:47:22 +0900

    user新規登録機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  private
+
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :email, :encrypted_password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :encrypted_password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
   end
 
   def basic_auth
