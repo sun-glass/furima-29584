dc9e9d1951c0ccd9cfd6e00e506ed757ba814ee4
sun-glass <68639801+sun-glass@users.noreply.github.com>
2020-12-18 03:09:20 +0900

    with_optionsでバリデーションをまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nick_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birth_date, presence: true
+  with_options presence: true do
+    validates :nick_name
+    validates :birth_date
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
