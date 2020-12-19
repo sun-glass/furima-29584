f293055c780d3f33af1172525cae358b72ecb5cb
sun-glass <68639801+sun-glass@users.noreply.github.com>
2020-12-19 21:17:36 +0900

    商品出品ページへのリンク設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def items_params
+    params.require(:item).permit(:name, :text, :price).merge(user_id: current_user.id)
+  end
+
 end
