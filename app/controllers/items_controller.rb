0ecd4c363494a07b1f29a5e0a46bb67b81b0e6a6
sun-glass <68639801+sun-glass@users.noreply.github.com>
2020-12-21 15:11:13 +0900

    ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,15 +3,22 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @items.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
-  def items_params
-    params.require(:item).permit(:name, :text, :price).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:name, :text, :price, :image, :category_id).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,20 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :price
+  end
+  
+  with_options presence: true, numericality: { other_than: 1 } do
+  validates :category_id
+  validates :status_id
+  validates :prefecture
+  end
+
+  belongs_to :user
+  #has_one_attached :
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :Status, :delivery_fee, :Prefecture, :shipping_day
+
 end
