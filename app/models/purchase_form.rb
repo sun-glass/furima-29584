8c8d6f9eb46746e7850e258f70bcba292935b7c0
sun-glass <68639801+sun-glass@users.noreply.github.com>
2021-02-01 18:38:55 +0900

    購入機能モデルの単体テスト

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -6,7 +6,7 @@ class PurchaseForm
     validates :post_code, format: {with: /\A\d{3}[-]\d{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
-    validates :block
+    validates :block, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
     validates :phon_number, numericality: { only_integer: true },length: { in: 10..11 }
     validates :token
     validates :user_id
